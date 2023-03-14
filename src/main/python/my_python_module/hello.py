65a3e520148f4f4a482a3ee6354a66dc15851c57
ravinayag <ravinayag@gmail.com>
2023-03-14 20:01:22 +0530

    first commit

---
+++ src/main/python/my_python_module/hello.py
@@ -0,0 +1,17 @@
+
+def hello_Bohus():
+    print('Hello, Bohus!, Executing  python file !!!')
+hello_Bohus()
+
+import numpy as np
+  
+  
+# Create an empty array
+empa = np.empty((3, 4), dtype=int)
+print("Empty Array")
+print(empa)
+  
+# Create a full array
+flla = np.full([3, 3], 55, dtype=int)
+print("\n Full Array")
+print(flla)
---
+++ src/main/python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_python_module',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.21.0",
+        "pandas"
+    ],
+)
+
+
